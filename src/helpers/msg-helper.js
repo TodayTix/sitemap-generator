61a2156fc2369d16ca50da49bcf289688ccb2e03
Anthony Galvin <anthony@todaytixgroup.com>
2025-09-26 12:09:44 +0100

    update chalk usage

--- src/helpers/msg-helper.js
+++ src/helpers/msg-helper.js
@@ -1,4 +1,22 @@
-const chalk = require('chalk');
+let chalk = {
+  magenta: (s) => s,
+  yellow: (s) => s,
+  yellowBright: (s) => s,
+  green: (s) => s,
+  blue: (s) => s,
+  red: (s) => s,
+  white: (s) => s,
+};
+
+// Dynamically import chalk (ESM-only) if present
+(async () => {
+  try {
+    const mod = await import('chalk');
+    chalk = mod.default || mod;
+  } catch (err) {
+    // Fallback: leave chalk as no-op functions
+  }
+})();
 
 module.exports.appMsg = function (msg) {
   console.log(chalk.magenta(msg));
@@ -21,4 +39,3 @@ module.exports.error = function (msg) {
 module.exports.info = function (msg) {
   console.log(chalk.white(msg));
 };
-
