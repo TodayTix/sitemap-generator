27daaf9481a32a2ea181fcae689fc8884f04bd8e
Anthony Galvin <anthony@todaytixgroup.com>
2025-09-26 11:42:53 +0100

    fix up old approach with newer native node options

--- src/SitemapStream.js
+++ src/SitemapStream.js
@@ -1,12 +1,17 @@
 const path = require('path');
-const rand = require('crypto-random-string');
+const { randomBytes } = require('crypto'); // ✅ replace crypto-random-string
 const os = require('os');
 const fs = require('fs');
 const escapeUnsafe = require('./helpers/escapeUnsafe');
 const msg = require('./helpers/msg-helper');
 
+// Helper to mimic crypto-random-string
+function rand(length = 10) {
+  return randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+}
+
 module.exports = function SitemapStream(options) {
-  const tmpPath = path.join(os.tmpdir(), `sitemap_${rand({length: 10})}`);
+  const tmpPath = path.join(os.tmpdir(), `sitemap_${rand(10)}`);
   msg.info('USING TMP PATH TO SAVE SITEMAP: ' + tmpPath);
 
   const stream = fs.createWriteStream(tmpPath);
@@ -14,29 +19,30 @@ module.exports = function SitemapStream(options) {
 
   const getPath = () => tmpPath;
 
-  const getPiriorityFromDepth = depth => {
+  const getPriorityFromDepth = depth => {
     let pir = 0.5;
-    let zeroIndexedDepth = depth - 1;
+    const zeroIndexedDepth = depth - 1;
     if (zeroIndexedDepth === 0) {
       pir = 1;
     } else if (zeroIndexedDepth === 1) {
       pir = 0.9;
     } else if (zeroIndexedDepth === 2) {
-      pir = .8;
+      pir = 0.8;
     } else if (zeroIndexedDepth === 3) {
-      pir = .7;
+      pir = 0.7;
     } else if (zeroIndexedDepth === 4) {
-      pir = .6;
+      pir = 0.6;
     }
     return pir;
   };
+
   const addURL = url => {
     urls.push(url);
   };
 
   const initXML = () => {
     stream.write('<?xml version="1.0" encoding="utf-8" standalone="yes" ?>');
-    stream.write('\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" >');
+    stream.write('\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">');
   };
 
   const flushURL = (queueItem) => {
@@ -45,25 +51,23 @@ module.exports = function SitemapStream(options) {
     const escapedUrl = escapeUnsafe(queueItem.url);
 
     stream.write(`\n  <url>\n    <loc>${escapedUrl}</loc>`);
-    for (let alternativeUrl of queueItem.alternatives) {
-      // Skip self refrence alternative URL
-      // if(alternativeUrl.value === url.value){
-      //   continue;
-      // }
-      stream.write(`\n    <xhtml:link rel='alternate' hreflang='` + alternativeUrl.lang +
-        `' href='` + escapeUnsafe(alternativeUrl.url) + `' />`);
+    for (const alternativeUrl of queueItem.alternatives) {
+      // Skip self reference alternative URL if needed
+      stream.write(
+        `\n    <xhtml:link rel='alternate' hreflang='${alternativeUrl.lang}' href='${escapeUnsafe(alternativeUrl.url)}' />`
+      );
     }
-    if(options.changeFreq){
-      stream.write(`\n    <changefreq>` + options.changeFreq + `</changefreq>`);
+    if (options.changeFreq) {
+      stream.write(`\n    <changefreq>${options.changeFreq}</changefreq>`);
     }
-    stream.write(`\n    <priority>` + getPiriorityFromDepth(queueItem.depth) + `</priority>`);
-    stream.write(`\n    <lastmod>` + queueItem.lastMod + `</lastmod>`);
+    stream.write(`\n    <priority>${getPriorityFromDepth(queueItem.depth)}</priority>`);
+    stream.write(`\n    <lastmod>${queueItem.lastMod}</lastmod>`);
     stream.write(`\n  </url>`);
   };
+
   const flush = () => {
     initXML();
-
-    for (let url of urls) {
+    for (const url of urls) {
       flushURL(url);
     }
   };
